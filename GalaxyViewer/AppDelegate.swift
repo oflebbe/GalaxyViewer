6f83279c42a428dcb7e9dbb8b69d4bb277c6b116
Olaf Flebbe <of@oflebbe.de>
2020-10-03 11:27:57 +0200

    Initial Commit

---
+++ GalaxyViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GalaxyViewer
+//
+//  Created by Olaf Flebbe on 03.10.20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
